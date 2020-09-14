d112bb311b48ebc436de35a4bdbd6dc95261bf64
Samyak-code <66072976+Samyak-code@users.noreply.github.com>
2020-09-14 09:35:14 +0530

    Update Whatsapp API.py

--- Whatsapp API.py
+++ Whatsapp API.py
@@ -1,13 +1,14 @@
+#First type "pip install twilio" for windows in cmd
+#make an account on twilio sandbox
 from twilio.rest import Client 
- 
 account_sid = 'ACb4279943a0a58042ebf1195b2fb4a953' 
-auth_token = 'a387d42f03d9985163aa2852e6383ffa' 
+auth_token = 'Enter your own authentication code. I cannot share mine due to security purposes.' 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
-                              body='Poochi',      
-                              to='whatsapp:+919999164111' 
+                              body='Hello World',      
+                              to='whatsapp:+919999164111'#your mobile number 
                           ) 
  
 print(message.sid)
